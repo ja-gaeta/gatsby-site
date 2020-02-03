72645cda764e33a69aab27072c9c6eb178966c61
Jose Mendes <ja.gaeta@hotmail.com>
2020-02-03 17:21:01 -0300

    Fixed featured tours

---
+++ src/components/Tours/TourList.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react"
+import styles from "../../css/items.module.css"
+import Tour from "./Tour"
+import Title from "../Title"
+
+export default class TourList extends Component {
+  state = {
+    tours: [],
+    sortedTours: [],
+  }
+
+  componentDidMount() {
+    this.setState({
+      tours: this.props.tours.edges,
+      sortedTours: this.props.tours.edges,
+    })
+  }
+
+  render() {
+    return (
+      <section className={styles.tours}>
+        <Title title="our" subtitle="tours" />
+      </section>
+    )
+  }
+}
